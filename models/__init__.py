4f53718ce515cab19b26127d7f7d9dad9675f066
Santiago Lizardo <santiago.lizardo@gmail.com>
2015-06-07 12:57:24 +0100

    Registration time column add to the score table

--- models/__init__.py
+++ models/__init__.py
@@ -2,15 +2,20 @@
 from google.appengine.ext import ndb
 
 class Player( ndb.Model ):
-	pass
+    pass
 
 class Score( ndb.Model ):
-	game_name = ndb.StringProperty()
-        game_level_number = ndb.IntegerProperty()
-	player_name = ndb.StringProperty()
-	value = ndb.IntegerProperty()
+    game_name = ndb.StringProperty()
+    game_level_number = ndb.IntegerProperty()
+    player_name = ndb.StringProperty()
+    value = ndb.IntegerProperty()
+    registration_time = ndb.DateTimeProperty( auto_now_add = True )
 
-	@classmethod
-	def find_by_game( cls, game_name, limit = 20 ):
-		return cls.query().filter( cls.game_name == game_name ).fetch( limit )
+    @classmethod
+    def find_by_game( cls, game_name, limit = 20 ):
+        return cls.query().filter( cls.game_name == game_name ).fetch( limit )
+
+class Game( ndb.Model ):
+    alias = ndb.StringProperty()
+    name = ndb.StringProperty()
 
